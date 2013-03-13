1c63ae852a6dba177bc4d59f7d50e8f72c9a3ed3
Chung Leong <chernyshevsky@hotmail.com>
2013-03-13 01:47:06 +0100

    Fixed clamp()

--- code_gen/Math/QBClampHandler.php
+++ code_gen/Math/QBClampHandler.php
@@ -5,12 +5,12 @@ class QBClampHandler extends QBHandler {
 	protected function getScalarExpression() {
 		$cType = $this->getOperandCType(3);
 		$lines = array();
-		$lines[] = "if(op3 < op1) {";
-		$lines[] =		"res = op1;";
-		$lines[] = "} else if(op3 > op2) {";
+		$lines[] = "if(op1 < op2) {";
 		$lines[] =		"res = op2;";
+		$lines[] = "} else if(op1 > op3) {";
+		$lines[] =		"res = op3;";
 		$lines[] = "} else {";
-		$lines[] = 		"res = op3;";
+		$lines[] = 		"res = op1;";
 		$lines[] = "}";
 		return $lines;;
 	}
--- tests/intrinsic-clamp.phpt
+++ tests/intrinsic-clamp.phpt
@@ -1,4 +1,20 @@
 --TEST--
 Clamp test
 --FILE--
+<?php
+
+/**
+ * @engine qb
+ * @param float64[]	$a
+ */
+function test_function($a) {
+	echo clamp($a, 10, 100), "\n";
+}
+
+qb_compile();
+
+test_function(array(1, 34, 999, 99, 17, 44, INF, NAN));
+
+?>
 --EXPECT--
+[10, 34, 100, 99, 17, 44, 100, NAN]
