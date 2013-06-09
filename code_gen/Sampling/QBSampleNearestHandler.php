893e9c1fe46a0035a8ac75f2bbad6694cea2da72
Chung Leong <chernyshevsky@hotmail.com>
2013-06-09 20:21:19 +0200

    Undoing the dumb "optimization," for real this time

--- code_gen/Sampling/QBSampleNearestHandler.php
+++ code_gen/Sampling/QBSampleNearestHandler.php
@@ -5,11 +5,12 @@ class QBSampleNearestHandler extends QBSampleHandler {
 	public function getHelperFunctions() {
 		$type = $this->getOperandType(1);
 		$cType = $this->getOperandCType(1);
+		$f = ($type == 'F32') ? 'f' : '';
 		$functions = array(
 			array(
 				"static void ZEND_FASTCALL qb_sample_nearest_3x_$type($cType *__restrict pixels, uint32_t width, uint32_t height, $cType x, $cType y, $cType *__restrict res_ptr) {",
-					"int32_t ix = qb_quick_floor(x);",
-					"int32_t iy = qb_quick_floor(y);",
+					"int32_t ix = qb_quick_floor$f(x);",
+					"int32_t iy = qb_quick_floor$f(y);",
 					"if(((uint32_t) ix < width) && ((uint32_t) iy < height)) {",
 						"uint32_t index = ((iy * width) + ix) * 3;",
 						"res_ptr[0] = pixels[index + 0];",
@@ -22,8 +23,8 @@ class QBSampleNearestHandler extends QBSampleHandler {
 			),
 			array(
 				"static void ZEND_FASTCALL qb_sample_nearest_4x_$type($cType *__restrict pixels, uint32_t width, uint32_t height, $cType x, $cType y, $cType *__restrict res_ptr) {",
-					"int32_t ix = qb_quick_floor(x);",
-					"int32_t iy = qb_quick_floor(y);",
+					"int32_t ix = qb_quick_floor$f(x);",
+					"int32_t iy = qb_quick_floor$f(y);",
 					"if(((uint32_t) ix < width) && ((uint32_t) iy < height)) {",
 						"uint32_t index = ((iy * width) + ix) * 4;",
 						"res_ptr[0] = pixels[index + 0];",
