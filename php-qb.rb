f351e7923045ee97d28ce3165235c23d99e4c650
Chung Leong <cleong@cal.berkeley.edu>
2013-06-16 02:44:24 +0200

    Ensured that gcc is selected, not clang

--- php-qb.rb
+++ php-qb.rb
@@ -2,9 +2,11 @@ require 'formula'
 
 class PhpQb < Formula
   homepage 'http://www.php-qb.net/'
-  url 'https://php-qb.googlecode.com/files/qb-1.2-source-code.tar.bz2'
-  sha1 '8474fa9475c4841d31791cc5abfa4784eb9c93e1'
+  url 'https://php-qb.googlecode.com/files/qb-1.3-source-code.tar.bz2'
+  sha1 'a9dda48aaae614835fe8d8a769dd8c612ad54232'
 
+  depends_on 'autoconf' => :build
+  
   def install
     system "phpize"
     system "./configure"
@@ -15,5 +17,10 @@ class PhpQb < Formula
 
   test do
     system "echo \"<?php exit(extension_loaded('qb') || dl('qb.so') ? 0 : -1); ?>\" | php"
+  end
+  
+  fails_with :clang do
+  	build 425
+    cause "Compiling with clang is incredibly slow"
   end
 end
