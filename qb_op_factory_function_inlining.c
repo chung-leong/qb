7ca0b7cd0a3ab4477868a1d63831adf54ce9dfb7
Chung Leong <chernyshevsky@hotmail.com>
2013-11-21 03:41:59 +0100

    Got function inlining to work partially

--- qb_op_factory_function_inlining.c
+++ qb_op_factory_function_inlining.c
@@ -131,7 +131,7 @@ static qb_variable * qb_find_variable_with_array_size_address(qb_compiler_contex
 	return NULL;
 }
 
-static void qb_set_writable_scalar_substitution(qb_compiler_context *cxt, qb_compiler_context *other_cxt, qb_address *other_address, qb_operand *arguments, uint32_t argument_count, qb_address_substitution *substitution) {
+static void qb_set_writable_scalar_substitution(qb_compiler_context *cxt, qb_compiler_context *other_cxt, qb_address *other_address, qb_operand *arguments, uint32_t argument_count, qb_operand *result, qb_address_substitution *substitution) {
 	qb_address *address;
 	qb_variable *other_var = qb_find_variable_with_address(other_cxt, other_address);
 	if(other_var) {
@@ -141,6 +141,12 @@ static void qb_set_writable_scalar_substitution(qb_compiler_context *cxt, qb_com
 				address = arguments[argument_index].address;
 			} else {
 				address = qb_obtain_constant_zval(cxt, other_var->default_value, other_var->address->type);
+			}
+		} else if(other_var->flags & QB_VARIABLE_RETURN_VALUE) {
+			if(result->type == QB_OPERAND_ADDRESS) {
+				address = result->address;
+			} else {
+				address = NULL;
 			}
 		} else {
 			if(other_var->flags & QB_VARIABLE_GLOBAL) {
@@ -155,9 +161,22 @@ static void qb_set_writable_scalar_substitution(qb_compiler_context *cxt, qb_com
 
 			}
 		}
+	} else {
+		address = qb_obtain_temporary_variable(cxt, other_address->type, NULL);
 	}
 	substitution->original = other_address;
 	substitution->current = address;
+}
+
+static qb_address * qb_find_substitution_address(qb_compiler_context *cxt, qb_address *address, qb_address_substitution *substitutions, uint32_t substitution_count) {
+	uint32_t i;
+	for(i = 0; i < substitution_count; i++) {
+		qb_address_substitution *substitution = &substitutions[i];
+		if(substitution->original == address) {
+			return substitution->current;
+		}
+	}
+	return NULL;
 }
 
 static void qb_inline_function(qb_compiler_context *cxt, void *factory, qb_operand *operands, uint32_t operand_count, qb_operand *result, uint32_t *jump_target_indices, uint32_t jump_target_count, qb_result_prototype *result_prototype) {
@@ -184,6 +203,17 @@ static void qb_inline_function(qb_compiler_context *cxt, void *factory, qb_opera
 		qb_perform_type_coercion(cxt, &arguments[i], arg_type, 0);
 	}
 
+	// get write target for return value
+	if(result->type != QB_OPERAND_NONE) {
+		qb_address *ret_address = other_cxt->return_variable->address;
+		if(ret_address) {
+			qb_variable_dimensions dim;
+			qb_copy_address_dimensions(other_cxt, ret_address, 0, &dim);
+			result->address = qb_obtain_write_target(cxt, ret_address->type, &dim, ret_address->flags, result_prototype, TRUE);
+			result->type = QB_OPERAND_ADDRESS;
+		}
+	}
+
 	substitution_count = other_cxt->constant_scalar_count + other_cxt->constant_array_count + other_cxt->writable_scalar_count + other_cxt->writable_array_count;
 	substitutions = do_alloca(sizeof(qb_address_substitution *) * substitution_count, use_heap);
 
@@ -200,7 +230,54 @@ static void qb_inline_function(qb_compiler_context *cxt, void *factory, qb_opera
 	// see how writable addresses should be mapped
 	for(i = 0; i < other_cxt->writable_scalar_count; i++, j++) {
 		qb_address *other_address = other_cxt->writable_scalars[i];
-		qb_set_writable_scalar_substitution(cxt, other_cxt, other_address, arguments, argument_count, &substitutions[j]);
+		qb_set_writable_scalar_substitution(cxt, other_cxt, other_address, arguments, argument_count, result, &substitutions[j]);
+	}
+
+	// copy the ops from the callee into the caller
+	for(i = 0; i < other_cxt->op_count; i++) {
+		qb_op *other_op = other_cxt->ops[i];
+		if(other_op->opcode == QB_RET) {
+			// don't need to worry about multiple return for now, since functions with branching are flagged as not-inlineable
+			break;
+		} else {
+			int32_t omit = FALSE;
+			qb_op *qop = qb_allocate_op(cxt->pool);
+			qop->flags = other_op->flags;
+			qop->line_number = cxt->line_number;
+			qop->opcode = other_op->opcode;
+			qop->operand_count = other_op->operand_count;
+			qop->operands = qb_allocate_operands(cxt->pool, qop->operand_count);
+
+			for(j = 0; j < qop->operand_count; j++) {
+				qb_operand *other_operand = &other_op->operands[j];
+				qb_operand *operand = &qop->operands[j];
+				if(other_operand->type == QB_OPERAND_ADDRESS) {
+					operand->address = qb_find_substitution_address(cxt, other_operand->address, substitutions, substitution_count);
+					operand->type = other_operand->type;
+					if(operand->address) {
+						if(!READ_ONLY(other_operand->address)) {
+							qb_mark_as_writable(cxt, operand->address);
+						}
+					} else {
+						// address is NULL--probably because the return value isn't used
+						omit = TRUE;
+					}
+				} else {
+					*operand = *other_operand;
+				}
+			}
+			if(!omit) {
+				qb_add_op(cxt, qop);
+			}
+		}
+	}
+
+	// release temporary variables
+	for(i = other_cxt->constant_scalar_count + other_cxt->constant_array_count; i < substitution_count; i++) {
+		qb_address_substitution *substitution = &substitutions[i];
+		if(TEMPORARY(substitution->current)) {
+			qb_unlock_address(cxt, substitution->current);
+		}
 	}
 
 	free_alloca(substitutions, use_heap);
