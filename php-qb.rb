e92ec18b5dbacf26f61d2a47213c18ee2a164ea3
Chung Leong <cleong@cal.berkeley.edu>
2014-03-30 01:54:21 +0100

    Update php-qb.rb

--- php-qb.rb
+++ php-qb.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PhpQb < Formula
   homepage 'http://www.php-qb.net/'
   url 'https://bitbucket.org/chung_leong/qb/downloads/qb-2.1-source-code.tar.bz2'
-  sha1 '4be8511f8165e1c275b62f9cfc1a434e6f216325'
+  sha1 'f5b571244e42d1e3bf1e0f75d0b0cb34de81ab1d'
 
   depends_on 'autoconf' => :build
   
