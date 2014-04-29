fd26365628f74bd09d49b96c3d4c57d6ef27f535
Chung Leong <cleong@cal.berkeley.edu>
2014-04-29 17:57:00 +0200

    Updated URL, SHA1
    Also removed check for clang

--- php-qb.rb
+++ php-qb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class PhpQb < Formula
   homepage 'http://www.php-qb.net/'
-  url 'https://bitbucket.org/chung_leong/qb/downloads/qb-2.1-source-code.tar.bz2'
-  sha1 'f5b571244e42d1e3bf1e0f75d0b0cb34de81ab1d'
+  url 'https://bitbucket.org/chung_leong/qb/downloads/qb-2.3-source-code.tar.bz2'
+  sha1 'edf7226bec15e48e676a6dccc5da846139be6139'
 
   depends_on 'autoconf' => :build
   
@@ -17,10 +17,5 @@ class PhpQb < Formula
 
   test do
     system "echo \"<?php exit(extension_loaded('qb') || dl('qb.so') ? 0 : -1); ?>\" | php"
-  end
-  
-  fails_with :clang do
-    build 425
-    cause "Compiling with clang is incredibly slow"
   end
 end
